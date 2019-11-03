bb197a43284997757a8cd5107abe2697c85900d8
mingyong.dev <mingyong.dev@gmail.com>
2019-11-02 22:47:29 -0400

    Updates

---
+++ precache-manifest.14204237392ed00ebccda397e460cb3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d66d61a4d333545dcd74b604732ebe1",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "419ff4968b95b7718e16",
+    "url": "/react-tic-tac-toe/static/css/main.8043d120.chunk.css"
+  },
+  {
+    "revision": "9cccfd98a4e6a33a0419",
+    "url": "/react-tic-tac-toe/static/js/2.e05e6227.chunk.js"
+  },
+  {
+    "revision": "419ff4968b95b7718e16",
+    "url": "/react-tic-tac-toe/static/js/main.4057a70a.chunk.js"
+  },
+  {
+    "revision": "ef1078d4c71916466003",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.fa4be67f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59581e880c8915b1f46951bf3a9847f5.js"
+  "/react-tic-tac-toe/precache-manifest.14204237392ed00ebccda397e460cb3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
